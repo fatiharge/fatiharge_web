ce79ef2fbb851ee361debb373e229305670962e6
fatihcetin <fatihcetin@tamfinans.com.tr>
2024-10-11 20:19:35 +0300

    feat : rm design

--- lib/product/design/component/atoms/spacer/spacer.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Spacer extends SizedBox {
-  const Spacer({super.key, double? height}) : super(height: height ?? 8);
-
-  const Spacer.vertical({super.key, double? width}) : super(width: width ?? 8);
-}
